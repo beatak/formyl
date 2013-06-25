56acd4aac23f9742971cf1a49f2705c9be3ae98f
Takashi Mizohata <beatak@gmail.com>
2013-06-24 17:55:13 -0700

    initial commit

---
+++ src/formyl.js
@@ -0,0 +1,15 @@
+var Formyl = (function () {
+  "use strict";
+  var Formyl = {};
+
+  Formyl.crate = function (name, option) {
+    
+  };
+
+  var Oneform = function () {
+    
+  };
+
+
+  return Formyl;
+})();
