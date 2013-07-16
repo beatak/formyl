3991891ba7290e6293e76c27c5aa3513d3e712bd
Takashi Mizohata <beatak@gmail.com>
2013-07-16 13:51:50 -0700

    organizing file structures

--- example/config.rb
+++ example/config.rb
@@ -6,7 +6,7 @@ css_dir = "css"
 sass_dir = "scss"
 #images_dir = "images"
 #javascripts_dir = "javascripts"
-# add_import_path "vendor/foundation/scss"
+add_import_path "../lib/scss"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
